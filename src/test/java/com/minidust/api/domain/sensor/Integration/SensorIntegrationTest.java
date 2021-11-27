027a8f1383761ca380ce55fa9b758bddda14fe7c
han-gyeong <hangyeong@outlook.com>
2021-11-28 00:15:48 +0900

    테스트 : mockMvc를 이용한 컨트롤러 테스트 추가, Sensor 테스트를 위한 객체 생성 함수 추가

--- src/test/java/com/minidust/api/domain/sensor/Integration/SensorIntegrationTest.java
+++ src/test/java/com/minidust/api/domain/sensor/Integration/SensorIntegrationTest.java
@@ -4,19 +4,15 @@ import com.minidust.api.domain.sensor.dto.SensorDto;
 import com.minidust.api.domain.sensor.models.Sensor;
 import com.minidust.api.domain.sensor.repository.SensorRepository;
 import com.minidust.api.domain.sensor.service.SensorService;
-import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
@@ -29,11 +25,8 @@ public class SensorIntegrationTest {
     @Autowired
     SensorService sensorService;
 
-    SensorDto sensorDto;
-
-    @Before
-    public void init() {
-        sensorDto = SensorDto.builder()
+    public SensorDto getSensorDto() {
+        return SensorDto.builder()
                 .id(2000)
                 .humidity(50)
                 .temperature(23)
@@ -47,22 +40,21 @@ public class SensorIntegrationTest {
     @Test
     public void 센서_업로드_정상() {
         // given
+        SensorDto sensorDto = getSensorDto();
 
         // when
         Sensor sensor = sensorService.upload(sensorDto);
 
         // then
-        Sensor sensor1 = sensorRepository.findById(sensorDto.getId()).get();
-        System.out.println("sensor = " + sensor);
-        System.out.println("sensor1 = " + sensor1);
+        Sensor sensor1 = sensorRepository.findById(getSensorDto().getId()).get();
         assertThat(sensor).isEqualTo(sensor1);
     }
 
     @Test
     public void 센서_업데이트_정상() {
         // given
-        Sensor sensor = sensorService.upload(sensorDto); // 센서를 업로드 하고
-        Sensor sensor1 = sensorRepository.findById(sensorDto.getId()).get(); // 업로드 된 센서가 주어졌을때
+        Sensor sensor = sensorService.upload(getSensorDto()); // 센서를 업로드 하고
+        Sensor sensor1 = sensorRepository.findById(getSensorDto().getId()).get(); // 업로드 된 센서가 주어졌을때
 
         // when
         SensorDto sensorDto2 = SensorDto.builder()
--- src/test/java/com/minidust/api/domain/sensor/controller/SensorControllerTest.java
+++ src/test/java/com/minidust/api/domain/sensor/controller/SensorControllerTest.java
@@ -1,35 +1,67 @@
 package com.minidust.api.domain.sensor.controller;
 
-import com.minidust.api.domain.sensor.models.Sensor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.minidust.api.domain.sensor.dto.SensorDto;
 import com.minidust.api.domain.sensor.service.SensorService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SensorController.class)
 class SensorControllerTest {
-
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @MockBean
     SensorService sensorService;
 
+
     @Test
-    void GET_ALL_정상작동() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/sensor"))
-                .andDo(print())
+    void 생성시도_정상작동() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/sensor/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(getRightSensorDto())))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void 생성시도_하지만_값오류() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/sensor/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(getWrongSensorDto())))
+                .andExpect(status().isBadRequest());
+    }
+
+    public SensorDto getRightSensorDto() {
+        return SensorDto.builder()
+                .id(2000)
+                .humidity(50)
+                .temperature(23)
+                .latitude(37.125)
+                .longitude(127.37)
+                .pm10(15)
+                .pm25(15)
+                .build();
+    }
+
+    public SensorDto getWrongSensorDto() {
+        return SensorDto.builder()
+                .id(2000)
+                .humidity(1000) // 값 범위 초과
+                .temperature(1000) // 값 범위 초과
+                .latitude(37.125)
+                .longitude(127.37)
+                .pm10(15)
+                .pm25(15)
+                .build();
     }
 }
