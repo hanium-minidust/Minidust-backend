1262e521f85cc71e2998bea3008239ac4c499a50
han-gyeong <hangyeong@outlook.com>
2022-03-06 15:52:34 +0900

    수정: 쿼리 파라미터 이름 수정

--- src/main/java/com/minidust/api/domain/pollution/controller/PollutionController.java
+++ src/main/java/com/minidust/api/domain/pollution/controller/PollutionController.java
@@ -24,24 +24,24 @@ public class PollutionController {
 
     @GetMapping
     @ApiOperation(value = "미세먼지 정보 조회", notes = "도시 이름을 받아 해당 도시 미세먼지 정보를 조회합니다.")
-    public ResponseEntity<Message> findByCityName(@RequestParam String cityName) {
+    public ResponseEntity<Message> findByCityName(@RequestParam("query") String cityName) {
         if (cityName.length() != 2) {
             throw new IllegalArgumentException("올바른 주소 이름 형식이 아닙니다. 예시) 서울, 경기");
         }
-        List<Pollution> result = pollutionService.findByCityName(cityName);
+        List<Pollution> result = pollutionService.findAllByName(cityName);
         return ResponseEntity.ok(Message.ok(result));
     }
 
     @GetMapping("/update/data")
     @ApiOperation(value = "미세먼지 정보 업데이트", notes = "미세먼지 정보 업데이트를 위한 컨트롤러 입니다.")
-    public ResponseEntity<Message> updateDust(@RequestParam String sidoName) {
+    public ResponseEntity<Message> updateDust(@RequestParam("query") String sidoName) {
         pollutionUpdateService.updateDust(sidoName);
         return ResponseEntity.ok(Message.ok());
     }
 
     @GetMapping("/update/station")
     @ApiOperation(value = "미세먼지 측정소 정보 업데이트", notes = "미세먼지 측정소 정보 업데이트를 위한 컨트롤러 입니다.")
-    public ResponseEntity<Message> updateStation(@RequestParam String sidoName) {
+    public ResponseEntity<Message> updateStation(@RequestParam("query") String sidoName) {
         pollutionUpdateService.updateStation(sidoName);
         return ResponseEntity.ok(Message.ok());
     }
